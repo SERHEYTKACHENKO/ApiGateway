58cd8df78480dd1ff093c63c32b8d6b6efabdc1e
SERHEYTKACHENKO <tsa3331@ukr.net>
2024-06-22 10:27:53 +0300

    Add files via upload

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,30 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Ocelot configuration
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+
+// Use Ocelot middleware
+app.UseOcelot().Wait();
+
+app.Run();
